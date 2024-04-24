042fb585132e33c2f0a96f970d5b93da67e94c0f
Gowtham A <91416588+GowthamA1963@users.noreply.github.com>
2024-04-24 16:40:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,25 +2,18 @@ import socket
 from rc4 import RC4
 
 def handle_client(client_socket, client_address, rc4_key):
-    """
-    Handle communication with a client.
-    
-    Args:
-        client_socket (socket.socket): The socket object for communication with the client.
-        client_address (tuple): A tuple representing the client's address (host, port).
-        rc4_key (RC4): An instance of the RC4 class for encryption and decryption.
-    """
     print(f"Connection from {client_address}")
     while True:
         data = client_socket.recv(4096)
         if not data:
             print(f"Client {client_address} disconnected.")
             break
-        decrypted_data = rc4_key.decrypt(data)  # Decrypt the received data
-        print(f"Received from {client_address}: {decrypted_data.decode('utf-8')}")
+        decrypted_data = rc4_key.decrypt(data)
+        decrypted_hex_data = " ".join([format(byte, "02x") for byte in decrypted_data])
+        print(f"Received from {client_address}: {decrypted_hex_data.upper()}")
         response = input("Enter response: ")  # Process the decrypted message
-        encrypted_response = rc4_key.encrypt(response.encode('utf-8'))  # Encrypt the response
-        client_socket.send(encrypted_response)  # Send the encrypted response
+        encrypted_response = rc4_key.encrypt(response.encode('utf-8'))
+        client_socket.send(encrypted_response)
     client_socket.close()
 
 def main():
